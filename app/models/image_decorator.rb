a595542a157edec635b03dd5c3d4689058009525
humancopy <info@humancopy.net>
2011-05-14 14:35:07 +0200

    Use S3 only in production

--- app/models/image_decorator.rb
+++ app/models/image_decorator.rb
@@ -1,8 +1,8 @@
 Image.class_eval do
   attachment_definitions[:attachment] = (attachment_definitions[:attachment] || {}).merge({
     :styles => { :mini => '48x48>', :small => '100x100>', :product => '240x240>', :large => '600x600>' },
-    :path => 'assets/products/:id/:style/:basename.:extension',
-    :storage => 's3',
+    :path => Rails.env.production? ? 'assets/products/:id/:style/:basename.:extension' : ':rails_root/public/assets/products/:id/:style/:basename.:extension',
+    :storage => Rails.env.production? ? 's3' : 'filesystem',
     :s3_credentials => Rails.root.join('config', 's3.yml')
   })
 end
